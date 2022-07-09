3cfece9a2c0e53e8649c7d6f5e59c1e4f8f0beea
GoochD4 <goochd411@gmail.com>
2022-07-09 17:00:47 +0000

    first commit

---
+++ ml-test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+i="1"
+
+while [ $i -lt 2000 ]
+do
+  echo $i
+  name=$(sort -R names.txt | head -n1)
+  extra=$(sort -R extra.txt | head -n1)
+  surname=$(sort -R surnames.txt | head -n1)
+  arroba="@"
+  domain=$(sort -R domains.txt | head -n1)
+  password=$(sort -R passwords.txt | head -n1)
+  email="$name$extra$surname$arroba$domain"
+  #echo #email
+  command='curl -d "&email='$email'&username= '$name' &password='$password'" -X POST https://www.choogyddad.com/DVWA/vulnerabilities/brute/'
+  echo $command
+  i=$[$i+1]
+done
